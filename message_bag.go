ff3a8a0a79cf790d57672e888df6f8a471c92acc
Weston Jossey <weston.jossey@gmail.com>
2014-09-30 10:00:20 -0400

    Add in the writer worker

--- message_bag.go
+++ message_bag.go
@@ -7,6 +7,7 @@ import (
   "time"
   "strconv"
   "math/rand"
+  "sync"
 )
 
 type MessageBag struct {
@@ -21,12 +22,12 @@ func main() {
   for i := 0; i < 0; i++ {
     go func() {
       messageBag := NewMessageBag()
-      for j := 0; j < 1000000; j++ {
-        _ = messageBag.Put("test_bucket", "Some simple message")    
+      for j := 0; j < 10000000; j++ {
+        _ = messageBag.Put("test_bucket", "Some simple message")
       }
     }()
   }
-  for i := 0; i < 1; i++ {
+  for i := 0; i < 0; i++ {
     for j := 0; j < 10000; j++ {
       //go func() {
         messageBag := NewMessageBag()
@@ -38,11 +39,53 @@ func main() {
       //}()    
     }            
   }
-  time.Sleep(100 * time.Second)
+  WriterBenchmark(1000, 100000000, 512)
   fmt.Printf("Complete\n")
 }
+  
+func rand_str(str_size int) string {
+    alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    var bytes = make([]byte, str_size)
+    for i, _ := range bytes {
+        bytes[i] = alphanum[rand.Intn(len(alphanum))]
+    }
+    return string(bytes)
+}
 
+func WriterBenchmark(workerCount int, numMessages int, messageSize int) {
+  //Process five messages at a time
+  messageChan := make(chan string, workerCount)
+  termSig := make(chan bool)
+  var wg sync.WaitGroup
+  for i := 0; i < workerCount; i++ {
+    wg.Add(1)
+    go WriterWorker(messageChan, termSig, &wg)
+  }
+  for i := 0; i < numMessages; i++ {
+    message := rand_str(messageSize)
+    messageChan <- message
+  }
+  for i := 0; i < workerCount; i++ {
+    termSig <- true
+  }
+  wg.Wait()
+}
 
+func WriterWorker(messageChan chan string, termChan chan bool, wg *sync.WaitGroup) {
+  messageBag := NewMessageBag()  
+
+  active := true  
+  for active == true {
+    select {
+      case msg := <- messageChan:    
+        _ = messageBag.Put("test_bucket", msg)
+      case sig := <- termChan:
+        fmt.Println("Close it out", sig)      
+        active = false
+    }
+  }  
+  wg.Done()
+}
 
 /* Begin Message Bag Def */
 
@@ -52,8 +95,8 @@ func NewMessageBag() *MessageBag {
 
 func NewClient() (*riak.Client, string) {
   rand.Seed(time.Now().UnixNano())
-  hosts := []string{"ec2-54-68-213-20.us-west-2.compute.amazonaws.com:8087", "ec2-54-68-12-138.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-47-53.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-46-124.us-west-2.compute.amazonaws.com:8087", "ec2-54-68-49-228.us-west-2.compute.amazonaws.com:8087", "ec2-54-68-118-225.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-49-89.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-49-183.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-50-238.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-29-87.us-west-2.compute.amazonaws.com:8087", "ec2-54-69-49-114.us-west-2.compute.amazonaws.com:8087", "ec2-54-68-187-187.us-west-2.compute.amazonaws.com:8087"}  
-  host := hosts[5]
+  hosts := []string{"10.0.0.108:8087", "10.0.0.155:8087", "10.0.0.165:8087", "10.0.0.187:8087", "10.0.0.208:8087", "10.0.0.212:8087", "10.0.0.253:8087", "10.0.0.30:8087", "10.0.0.47:8087", "10.0.0.71:8087", "10.0.0.86:8087", "10.0.0.97:8087"}  
+  host := hosts[rand.Intn(len(hosts))]
   client := riak.NewClient(host)
   client.SetConnectTimeout(2 * time.Second)
   err := client.Connect()
@@ -76,12 +119,13 @@ func (m *MessageBag) Put(bagName string, message string) string {
     if err == nil {
       //Retrieve a UUID
       rand.Seed(time.Now().UnixNano())
+      t := time.Now().UnixNano()
       randInt := rand.Intn(9999999999999999)
-      fmt.Println("Random int ", randInt)
       uuid := strconv.Itoa(randInt)
 
       messageObj := bucket.NewObject(uuid)
       messageObj.Indexes["id_int"] = []string{uuid}
+      messageObj.Indexes["inflight_int"] = []string{strconv.FormatInt(t, 10)}
       messageObj.Data = []byte(message)
       messageObj.Store()
       return uuid
@@ -112,10 +156,10 @@ func (m *MessageBag) Delete(bagName string, id string) string {
 var riakPool chan *riak.Client
 func GetConn() *riak.Client {
   if riakPool == nil {
-    fmt.Println("Initializing client pool")
-    riakPool = make(chan *riak.Client, 20)
-    for i := 0; i < 20; i++ {
-      fmt.Println("Initializing client pool ", i)
+    //fmt.Println("Initializing client pool")
+    riakPool = make(chan *riak.Client, 4096)
+    for i := 0; i < 4096; i++ {
+      //fmt.Println("Initializing client pool ", i)
       client, _ := NewClient()
       client.Ping()
       PutConn(client)
@@ -127,9 +171,9 @@ func GetConn() *riak.Client {
 
 func PutConn(conn *riak.Client) {
   if riakPool == nil {
-    riakPool = make(chan *riak.Client, 20)
+    riakPool = make(chan *riak.Client, 4096)
   }
-  fmt.Printf("Conn backlog %v", len(riakPool))
+  //fmt.Printf("Conn backlog %v", len(riakPool))
   riakPool <- conn
 }
 
@@ -138,18 +182,18 @@ func (m *MessageBag) GetMulti(bagName string, ids []string) []riak.RObject {
   var rKeys = make(chan string, len(ids))
 
   start := time.Now()
-  fmt.Println("In get multi") 
+  //fmt.Println("In get multi") 
   for i:=0; i < len(ids); i++ {
     go func() {
       var riakKey string
       client := GetConn()     
       defer PutConn(client)
-      fmt.Println("Getting bucket")
+      //fmt.Println("Getting bucket")
       bucket, _ := client.NewBucket(bagName)
       riakKey = <- rKeys
-      fmt.Println("Getting value")
+      //fmt.Println("Getting value")
       rObject, _ := bucket.Get(riakKey)
-      fmt.Println("Returning value")
+      //fmt.Println("Returning value")
       rObjects <- *rObject      
     }()
     rKeys <- ids[i]
@@ -158,7 +202,7 @@ func (m *MessageBag) GetMulti(bagName string, ids []string) []riak.RObject {
   for i:= 0; i < len(ids); i++ {      
     var obj = <- rObjects      
     returnVals = append(returnVals, obj)
-    fmt.Printf("\nGet Completed %v of %v", i, len(ids) - 1)
+    //fmt.Printf("\nGet Completed %v of %v", i, len(ids) - 1)
   }
   elapsed := time.Since(start)
   fmt.Printf("Get Multi Took %s\n", elapsed)
@@ -175,7 +219,7 @@ func (m *MessageBag) PutMulti(bagName string, riakObjects []riak.RObject) {
       client := GetConn()     
       defer PutConn(client)        
       riakObject := <- rObjectChan
-      fmt.Printf("Putting riak object %v\n", riakObject)
+      //fmt.Printf("Putting riak object %v\n", riakObject)
       riakObject.Store()
       completions <- true  
     }()
@@ -184,7 +228,7 @@ func (m *MessageBag) PutMulti(bagName string, riakObjects []riak.RObject) {
 
   for i:= 0; i < len(riakObjects); i++ {      
     _ = <- completions
-    fmt.Printf("\nPut Completed %v of %v\n", i, len(riakObjects) - 1)
+    //fmt.Printf("\nPut Completed %v of %v\n", i, len(riakObjects) - 1)
   }
   elapsed := time.Since(start)
   fmt.Printf("\nPut Multi Took %s\n", elapsed)
@@ -195,9 +239,9 @@ func (m *MessageBag) Get(bagName string) []riak.RObject {
   var returnMessages []riak.RObject    
   client := GetConn()    
   defer PutConn(client)
-  fmt.Println("Getting bucket")
+  //fmt.Println("Getting bucket")
   bucket, err := client.NewBucket(bagName)
-  fmt.Println("Bucket retrieved")
+  //fmt.Println("Bucket retrieved")
   if err == nil {
     //now := time.Now()
     //t := now.UnixNano()      
@@ -209,15 +253,13 @@ func (m *MessageBag) Get(bagName string) []riak.RObject {
     }
     base :=  baseDec * 9999999999999999
     max := highDec * 9999999999999999
-    fmt.Printf("Base: %v & Max: %v\n", base, max)
+    //fmt.Printf("Base: %v & Max: %v\n", base, max)
     start := time.Now()
-    fmt.Println("Retrieving messages")
+    //fmt.Println("Retrieving messages")
     messageIds, _, err := bucket.IndexQueryRangePage("id_int", strconv.FormatFloat(base, 'f', 0, 32), strconv.FormatFloat(max, 'f', 0, 32), 250, "")
-    fmt.Println("Message retrieved ", len(messageIds))
+    //fmt.Println("Message retrieved ", len(messageIds))
     elapsed := time.Since(start)
     fmt.Printf("id_int query took %s and returned %v messages \n", elapsed, len(messageIds))
-    length := len(messageIds)
-    fmt.Println("Length ", length)    
     if err == nil {          
       messages = m.GetMulti(bagName, messageIds)
       for _, message := range messages {
@@ -245,70 +287,6 @@ func (m *MessageBag) Get(bagName string) []riak.RObject {
   }
   
   return returnMessages
-}
-
-func (m *MessageBag) GetWithInflight(bagName string) []*riak.RObject {
-  messages := make([]*riak.RObject, 200)
-  client := m.client
-  err := client.Connect()
-  if err == nil {
-    bucket, err := client.NewBucket(bagName)
-    if err == nil {
-      now := time.Now()
-      t := now.UnixNano()
-      //Retrieve the inflight IDs
-      start := time.Now()
-      messageInFlightIds, err := bucket.IndexQueryRange("inflight_int", strconv.FormatInt(t - m.visibleTime, 10), strconv.FormatInt(t, 10))
-      elapsed := time.Since(start)
-      fmt.Printf("inflight_int query took %s %v\n", elapsed, len(messageInFlightIds))
-      //Retrieve inflight + 1 so you guarantee getting something (or nothing, if nothing is available)
-      //message_ids  = bag.get_index( 'id_bin', '0'..'z', max_results: (message_inflight_ids.count()+1))
-      rand.Seed(time.Now().UnixNano())
-      baseDec := rand.Float64()
-      highDec := baseDec + (1.0 / 512.0)
-      if highDec > 1 {
-        highDec = 1
-      }
-      base :=  baseDec * 9999999999999999
-      max := highDec * 9999999999999999
-      fmt.Println("Base: ", base)
-      start = time.Now()
-      messageIds, _, err := bucket.IndexQueryRangePage("id_int", strconv.FormatFloat(base, 'f', 0, 32), strconv.FormatFloat(max, 'f', 0, 32), 1, "")
-      elapsed = time.Since(start)
-      fmt.Printf("id_int query took %s\n", elapsed)
-      length := len(messageIds)
-      fmt.Println("Length ", length)    
-      messageSet := make(map[string]bool)
-      targetIds := make([]string, 0, length)
-      for i := 0; i < len(messageInFlightIds); i++ {
-        messageSet[messageInFlightIds[i]] = true
-      }
-
-      for i := 0; i < len(messageIds); i++ {
-        _, present := messageSet[messageIds[i]]
-        if present == false {
-          targetIds = append(targetIds, messageIds[i])
-        }
-      }
-      if err == nil {
-        //fmt.Printf("In flight IDs%v\n", messageInFlightIds)
-        //fmt.Printf("Message Set IDs%v\n", messageSet)
-        //fmt.Printf("Continuation %v\n", continuation)
-        fmt.Printf("Target id length %v\n", len(targetIds))
-        for _, messageId := range targetIds {
-          go func(){
-            message, _ := bucket.Get(messageId)
-            message.Indexes["inflight_int"] = []string{strconv.FormatInt(t, 10)}
-            message.Store()
-            messages = append(messages, message)
-          }()           
-        }
-      } else {
-        fmt.Printf("Error%v", err)
-      }
-    }
-  }
-  return messages
 }
 
 /* End Message Bag Def */
