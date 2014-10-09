19dccc50fd61c62651c1f9e51375aade16d7c4a5
James Moore <jcmmars@gmail.com>
2014-10-09 18:13:11 -0400

    fixing tabs/spaces

--- app/config.go
+++ app/config.go
@@ -1,33 +1,33 @@
 package app
 
 import (
-	"code.google.com/p/gcfg"
-	"log"
-	"time"
+  "code.google.com/p/gcfg"
+  "log"
+  "time"
 )
 
 type Config struct {
-	Core struct {
-		Name                  string
-		Port                  int
-		SeedServer            string
-		SeedPort              int
-		HttpPort              int
-		Visibility            float64
-		RiakNodes             string
-		BackendConnectionPool int
-		InitPartitions        int
-		MaxPartitions         int
-		PartitionStep         int
-		SyncConfigInterval    time.Duration
-	}
+  Core struct {
+    Name                  string
+    Port                  int
+    SeedServer            string
+    SeedPort              int
+    HttpPort              int
+    Visibility            float64
+    RiakNodes             string
+    BackendConnectionPool int
+    InitPartitions        int
+    MaxPartitions         int
+    PartitionStep         int
+    SyncConfigInterval    time.Duration
+  }
 }
 
 func Getconfig(config_file *string) (Config, error) {
-	var cfg Config
-	err := gcfg.ReadFileInto(&cfg, *config_file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return cfg, err
+  var cfg Config
+  err := gcfg.ReadFileInto(&cfg, *config_file)
+  if err != nil {
+    log.Fatal(err)
+  }
+  return cfg, err
 }
