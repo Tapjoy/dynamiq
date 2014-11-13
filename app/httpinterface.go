3334b66e46728860fd5fcec6180bbe9581ea5947
James Moore <jcmmars@gmail.com>
2014-11-13 13:56:27 -0500

    removing status call

--- app/httpinterface.go
+++ app/httpinterface.go
@@ -30,16 +30,6 @@ func InitWebserver(list *memberlist.Memberlist, cfg Config) {
     }
     return return_string
   })
-  m.Get("/status/:queue/partitions", func(r render.Render, params martini.Params) {
-
-    //check if we've initialized this queue yet
-    var present bool
-    _, present = queues.QueueMap[params["queue"]]
-    if present != true {
-      queues.InitQueue(cfg, params["queue"])
-    }
-    r.JSON(200, map[string]interface{}{"paritions": queues.QueueMap[params["queue"]].Parts.PartitionCount()})
-  })
   m.Get("/topics", func(r render.Render) {
     topicList := make([]string, 0, 10)
     for topicName, _ := range topics.TopicMap {
