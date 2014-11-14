eb96fe43a187071739ceea175816a6768f6162df
James Moore <jcmmars@gmail.com>
2014-11-14 14:53:05 -0500

    initial buggy! heap implementation

--- app/partitions.go
+++ app/partitions.go
@@ -3,28 +3,27 @@ package app
 import (
   "errors"
   "github.com/hashicorp/memberlist"
+  "github.com/oleiade/lane"
   "log"
   "math"
+  "math/rand"
   "sort"
   "strconv"
-  "sync"
   "time"
 )
 
 type Partitions struct {
-  partitions map[int]*Partition
-  sync.Mutex
+  partitions *lane.PQueue
 }
 
 type Partition struct {
   Id       int
   LastUsed time.Time
-  sync.Mutex
 }
 
 func InitPartitions(cfg Config) Partitions {
   part := Partitions{
-    partitions: make(map[int]*Partition),
+    partitions: lane.NewPQueue(lane.MINPQ),
   }
   //make sure there is an element in the array
   part.makePartitions(cfg, cfg.Core.InitPartitions)
@@ -32,7 +31,7 @@ func InitPartitions(cfg Config) Partitions {
 }
 
 func (part Partitions) PartitionCount() int {
-  return len(part.partitions)
+  return part.partitions.Size()
 }
 
 func (part Partitions) GetPartition(cfg Config, list *memberlist.Memberlist) (int, int, error) {
@@ -58,10 +57,10 @@ func (part Partitions) GetPartition(cfg Config, list *memberlist.Memberlist) (in
   log.Println("Node Range: " + strconv.Itoa(node_range))
   nodeStep := node_range / totalPartitions
   log.Println("nodeStep: " + strconv.Itoa(nodeStep))
-  partitionBottom := nodeStep * myPartition + nodeBottom
+  partitionBottom := nodeStep*myPartition + nodeBottom
   log.Println("my partition: " + strconv.Itoa(myPartition))
   log.Println("partitionBottom: " + strconv.Itoa(partitionBottom))
-  partitionTop := nodeStep * (myPartition + 1) + nodeBottom
+  partitionTop := nodeStep*(myPartition+1) + nodeBottom
   log.Println("partitionTop: " + strconv.Itoa(partitionTop))
   return partitionBottom, partitionTop, err
 
@@ -89,49 +88,41 @@ func (part Partitions) getPartitionPosition(cfg Config) (int, int, error) {
 
   //TODO move loging out of the sync operation for better throughput
   myPartition := -1
-  occupiedPartitions := 0
-  totalPartitions := len(part.partitions)
+  totalPartitions := part.partitions.Size()
+  log.Println("totalPartitions:" + strconv.Itoa(totalPartitions))
+
   var err error
-  for partitionId := range part.partitions {
-    //use visibility timeout of 30 seconds
-    log.Println("partition: " + strconv.Itoa(partitionId) + "occupied time: " + strconv.FormatFloat(time.Since(part.partitions[partitionId].LastUsed).Seconds(), 'f', -1, 64))
-    part.partitions[partitionId].Lock()
-    if time.Since(part.partitions[partitionId].LastUsed).Seconds() > cfg.Core.Visibility {
-      if myPartition == -1 {
-        myPartition = partitionId
-        part.partitions[partitionId].LastUsed = time.Now()
+  poppedPartition, _ := part.partitions.Pop()
+  workingPartition := poppedPartition.(*Partition)
+  log.Println("partition: " + strconv.Itoa(workingPartition.Id) + " occupied time: " + strconv.FormatFloat(time.Since(workingPartition.LastUsed).Seconds(), 'f', -1, 64))
 
-      }
-    } else {
-      occupiedPartitions = occupiedPartitions + 1
-    }
-    part.partitions[partitionId].Unlock()
-  }
-  //if I haven't found an unoccupied partition create more
-  if myPartition == -1 {
+  if time.Since(workingPartition.LastUsed).Seconds() > cfg.Core.Visibility {
+    myPartition = workingPartition.Id
+    workingPartition.LastUsed = time.Now()
+    part.partitions.Push(workingPartition, workingPartition.LastUsed.UnixNano())
+  } else {
 
+    part.partitions.Push(workingPartition, workingPartition.LastUsed.UnixNano())
     err = part.makePartitions(cfg, cfg.Core.PartitionStep)
     if err == nil {
       myPartition, totalPartitions, err = part.getPartitionPosition(cfg)
     }
   }
   log.Println("totalPartitions:" + strconv.Itoa(totalPartitions))
-  log.Println("occupiedPartitions:" + strconv.Itoa(occupiedPartitions))
   return myPartition, totalPartitions, err
 }
 
 func (part Partitions) makePartitions(cfg Config, partitionsToMake int) error {
-  part.Lock()
-  defer part.Unlock()
-  offset := len(part.partitions)
-  var initialTime time.Time
+  offset := part.partitions.Size()
   partitionsMade := 0
   for partitionId := offset; partitionId < offset+partitionsToMake; partitionId++ {
+    timeOffSet := -1 * rand.Int63n(1000000000000000000)
+
     if cfg.Core.MaxPartitions > partitionId {
       partition := new(Partition)
       partition.Id = partitionId
-      partition.LastUsed = initialTime
-      part.partitions[partitionId] = partition
+      partition.LastUsed = time.Now().Add(time.Duration(timeOffSet))
+      part.partitions.Push(partition, rand.Int63n(100000))
       partitionsMade = partitionsMade + 1
     }
   }
