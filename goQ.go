7aff817dbecce2e2b0b7544f938aeb7cc9465e1f
James Moore <jcmmars@gmail.com>
2014-11-20 17:03:34 -0500

    making some refactors for tests

--- goQ.go
+++ goQ.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  "./app/."
-  "flag"
-  "log"
+	"flag"
+	"github.com/Tapjoy/riakQueue/app"
+	"log"
 )
 
 func main() {
-  //Get some Command line options
-  config_file := flag.String("c", "./lib/config.gcfg", "location of config file")
-  flag.Parse()
+	//Get some Command line options
+	config_file := flag.String("c", "./lib/config.gcfg", "location of config file")
+	flag.Parse()
 
-  //setup the config file
-  cfg, err := app.Getconfig(config_file)
-  if err != nil {
-    log.Fatal(err)
-  }
+	//setup the config file
+	cfg, err := app.Getconfig(config_file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  list := app.InitMember(cfg)
+	list := app.InitMember(cfg)
 
-  app.InitWebserver(list, cfg)
+	app.InitWebserver(list, cfg)
 }
