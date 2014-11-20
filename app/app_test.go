206580b6dc834f5654e59f10400c58e54eb32d08
James Moore <jcmmars@gmail.com>
2014-11-20 17:06:14 -0500

    spacing!!

---
+++ app/app_test.go
@@ -0,0 +1,12 @@
+package app_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestPartitions(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "App Suite")
+}
