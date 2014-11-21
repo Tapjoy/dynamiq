0737fbf43f264c8f68b97776e4a604a5fa3c4778
StabbyCutyou <sean.kelly@tapjoy.com>
2014-11-21 09:41:16 -0500

    Coverage for InitPartitions

--- app/app_suite_test.go
+++ app/app_suite_test.go
@@ -1,11 +1,11 @@
 package app_test
 
 import (
-  . "github.com/Tapjoy/riakQueue/app"
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
-  "testing"
-  "time"
+	. "github.com/Tapjoy/riakQueue/app"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+	"time"
 )
 
 var cfg Config
@@ -13,24 +13,24 @@ var core Core
 var duration time.Duration
 
 func TestPartitions(t *testing.T) {
-  RegisterFailHandler(Fail)
-  RunSpecs(t, "App Suite")
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "App Suite")
 
 }
 
 var _ = BeforeSuite(func() {
-  core = Core{
-    Name:                  "john",
-    Port:                  8000,
-    SeedServer:            "steve",
-    SeedPort:              8001,
-    HttpPort:              8003,
-    Visibility:            30,
-    RiakNodes:             "localhost",
-    BackendConnectionPool: 16,
-    InitPartitions:        50,
-    MaxPartitions:         50,
-    SyncConfigInterval:    duration,
-  }
-  cfg.Core = core
+	core = Core{
+		Name:                  "john",
+		Port:                  8000,
+		SeedServer:            "steve",
+		SeedPort:              8001,
+		HttpPort:              8003,
+		Visibility:            30,
+		RiakNodes:             "localhost",
+		BackendConnectionPool: 16,
+		InitPartitions:        50,
+		MaxPartitions:         50,
+		SyncConfigInterval:    duration,
+	}
+	cfg.Core = core
 })
--- app/partitions_test.go
+++ app/partitions_test.go
@@ -1,14 +1,15 @@
 package app_test
 
 import (
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
+	. "github.com/Tapjoy/riakQueue/app"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Partition", func() {
-  Context("InitPartitions", func() {
-    It("should return a number of partitions equal to the configured amount", func() {
-      Expect(cfg.Core.InitPartitions).To(Equal(50))
-    })
-  })
+	Context("InitPartitions", func() {
+		It("should return a number of partitions equal to the configured amount", func() {
+			Expect(InitPartitions(cfg).PartitionCount()).To(Equal(50))
+		})
+	})
 })
