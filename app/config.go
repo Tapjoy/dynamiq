ef75e6fd5b7bf2afad51af8947f4cd4e241b86d2
StabbyCutyou <sean.kelly@tapjoy.com>
2014-11-24 14:41:36 -0500

    Slight refactoring of Getconfig to prepare for more config options

--- app/config.go
+++ app/config.go
@@ -1,33 +1,36 @@
 package app
 
 import (
-  "code.google.com/p/gcfg"
-  "log"
-  "time"
+	"code.google.com/p/gcfg"
+	"log"
+	"time"
 )
 
+// The Config / ConfigCore relationship is a required aspect of gcfg
+// It defines configuration in terms of sections, Core being the primary config
+// needed to start the process itself
 type Config struct {
-  Core Core
+	Core Core
 }
 type Core struct {
-  Name                  string
-  Port                  int
-  SeedServer            string
-  SeedPort              int
-  HttpPort              int
-  Visibility            float64
-  RiakNodes             string
-  BackendConnectionPool int
-  InitPartitions        int
-  MaxPartitions         int
-  SyncConfigInterval    time.Duration
+	Name                  string
+	Port                  int
+	SeedServer            string
+	SeedPort              int
+	HttpPort              int
+	Visibility            float64
+	RiakNodes             string
+	BackendConnectionPool int
+	InitPartitions        int
+	MaxPartitions         int
+	SyncConfigInterval    time.Duration
 }
 
-func Getconfig(config_file *string) (Config, error) {
-  var cfg Config
-  err := gcfg.ReadFileInto(&cfg, *config_file)
-  if err != nil {
-    log.Fatal(err)
-  }
-  return cfg, err
+func GetCoreConfig(config_file *string) (Config, error) {
+	var cfg Config
+	err := gcfg.ReadFileInto(&cfg, *config_file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cfg, err
 }
