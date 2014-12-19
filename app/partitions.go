d278b4d84f5b504537471716685512dc8fe99ae4
James Moore <jcmmars@gmail.com>
2014-12-19 12:22:25 -0500

    fix/LessLogging

--- app/partitions.go
+++ app/partitions.go
@@ -95,7 +95,6 @@ func (part *Partitions) getPartitionPosition(cfg *Config, queueName string) (int
 		// this seems a little scary
 		return myPartition, workingPartition, part.partitionCount, errors.New("no available partitions")
 	}
-	//log.Println("partition: " + strconv.Itoa(workingPartition.Id) + " occupied time: " + strconv.FormatFloat(time.Since(workingPartition.LastUsed).Seconds(), 'f', -1, 64))
 	visTimeout, _ := cfg.GetVisibilityTimeout(queueName)
 	if time.Since(workingPartition.LastUsed).Seconds() > visTimeout {
 		myPartition = workingPartition.Id
@@ -153,7 +152,6 @@ func (part *Partitions) syncPartitions(cfg *Config, queueName string) {
 		_, _ = part.partitions.Pop()
 	}
 	part.partitionCount = part.partitionCount - partsRemoved
-	log.Println("removed " + strconv.Itoa(partsRemoved) + "partitions from queue " + queueName)
 
 	if part.partitionCount < minPartitions {
 		part.makePartitions(cfg, queueName, minPartitions-part.partitionCount)
