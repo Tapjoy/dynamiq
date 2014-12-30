e174b869dea8503fde2bcd83962682e3ce9ba275
James Moore <jcmmars@gmail.com>
2014-12-30 11:47:33 -0500

    moving messages into their own type

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 riak-admin bucket-type create maps '{"props":{"datatype":"map"}}'
 riak-admin bucket-type activate maps
+riak-admin bucket-type create messages
+riak-admin bucket-type activate messages
