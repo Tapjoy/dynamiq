867dfcfc31f9c0492fe5803848d116e175358488
StabbyCutyou <sean.kelly@tapjoy.com>
2015-04-02 17:06:52 -0400

    Removing dead code

--- app/members.go
+++ app/members.go
@@ -7,22 +7,6 @@ import (
 	"strconv"
 )
 
-func InitMember(cfg *Config) *memberlist.Memberlist {
-	memberConf := memberlist.DefaultLANConfig()
-	memberConf.Name = cfg.Core.Name
-	memberConf.BindPort = cfg.Core.Port
-
-	list, err := memberlist.Create(memberConf)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-	list.Join([]string{(cfg.Core.SeedServer + ":" + strconv.Itoa(cfg.Core.SeedPort))})
-	for _, member := range list.Members() {
-		logrus.Printf("Member: %s %s\n", member.Name, member.Addr)
-	}
-	return list
-}
-
 func InitMemberList(name string, port int, seedServers []string, seedPort int) (*memberlist.Memberlist, int, error) {
 	conf := memberlist.DefaultLANConfig()
 	conf.Name = name
@@ -76,11 +60,4 @@ func prioritizeSeedServers(name string, seedServers []string) []string {
 	postSlice := seedServers[myPos+1 : len(seedServers)]
 
 	return append(postSlice, preSlice...)
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
 }
